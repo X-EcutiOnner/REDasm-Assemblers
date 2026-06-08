fcd849553fce689b6d404800a1c5bd808d005c68
Anton Bolshakov <blshkv@gmail.com>
2026-06-08 12:17:50 +0800

    cmake: prefer system capstone and zydis over CPM downloads
    Fixes #5
    
    Try find_package() for capstone and zydis before falling back to CPM.
    When the system provides these libraries, CPM downloads are skipped
    entirely. Aliases are created to map the system targets
    (capstone::capstone_shared, Zydis::Zydis) to the bare names
    (capstone, Zydis) that the targets already expect.

--- capstonebundle/cmake/Dependencies.cmake
+++ capstonebundle/cmake/Dependencies.cmake
@@ -1,14 +1,23 @@
 include(cmake/CPM.cmake)
 
 function(setup_dependencies)
-    CPMAddPackage(
-        NAME capstone
-        GIT_TAG "6.0.0-Alpha7"
-        GITHUB_REPOSITORY "capstone-engine/capstone"
+    find_package(capstone QUIET)
 
-        OPTIONS 
-            "CAPSTONE_ARCHITECTURE_DEFAULT OFF"
-            "CAPSTONE_ARM_SUPPORT ON"
-            "CAPSTONE_AARCH64_SUPPORT ON"
-    )
+    if(capstone_FOUND)
+        if(NOT TARGET capstone)
+            add_library(capstone ALIAS capstone::capstone_shared)
+        endif()
+        message(STATUS "Using system capstone")
+    else()
+        CPMAddPackage(
+            NAME capstone
+            GIT_TAG "6.0.0-Alpha7"
+            GITHUB_REPOSITORY "capstone-engine/capstone"
+
+            OPTIONS 
+                "CAPSTONE_ARCHITECTURE_DEFAULT OFF"
+                "CAPSTONE_ARM_SUPPORT ON"
+                "CAPSTONE_AARCH64_SUPPORT ON"
+        )
+    endif()
 endfunction()
--- x86/cmake/Dependencies.cmake
+++ x86/cmake/Dependencies.cmake
@@ -1,15 +1,24 @@
 include(cmake/CPM.cmake)
 
 function(setup_dependencies)
-    CPMAddPackage(
-        NAME zydis
-        VERSION "4.1.1"
-        GITHUB_REPOSITORY "zyantific/zydis"
-        EXCLUDE_FROM_ALL YES
+    find_package(zydis QUIET)
 
-        OPTIONS 
-            "CMAKE_POSITION_INDEPENDENT_CODE ON"
-            "ZYDIS_BUILD_TOOLS OFF"
-            "ZYDIS_BUILD_EXAMPLES OFF"
-    )
+    if(zydis_FOUND)
+        if(NOT TARGET Zydis)
+            add_library(Zydis ALIAS Zydis::Zydis)
+        endif()
+        message(STATUS "Using system zydis")
+    else()
+        CPMAddPackage(
+            NAME zydis
+            VERSION "4.1.1"
+            GITHUB_REPOSITORY "zyantific/zydis"
+            EXCLUDE_FROM_ALL YES
+
+            OPTIONS 
+                "CMAKE_POSITION_INDEPENDENT_CODE ON"
+                "ZYDIS_BUILD_TOOLS OFF"
+                "ZYDIS_BUILD_EXAMPLES OFF"
+        )
+    endif()
 endfunction()
